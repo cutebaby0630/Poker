c78769019bba25d38bd4c0218352998180713ed1
cutebaby0630 <lovemath0630@gmail.com>
2018-12-10 21:37:54 +0800

    AJQK

--- app/src/main/java/com/cutebaby/poker/Cards.java
+++ app/src/main/java/com/cutebaby/poker/Cards.java
@@ -5,10 +5,23 @@ public class Cards {
     int num;
 
     public Cards(int num) {
+
         this.num = num;
     }
 
     public String get() {
-        return num % 13 + 1 + " " + color[num / 13];
+        switch (num % 13 + 1 ){
+            case 1:
+                return "A" + " " + color[num / 13];
+            case 11:
+                return "J" + " " + color[num / 13];
+            case 12:
+                return "Q" + " " + color[num / 13];
+            case 13:
+                return "K" + " " + color[num / 13];
+                default:
+                    return num % 13 + 1 + " " + color[num / 13];
+        }
+//        return num % 13 + 1 + " " + color[num / 13];
     }
 }
